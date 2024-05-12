5a37b3312d5a195e01e31fc93a45260e9f2ec65e
satyaki95 <www.satyaki95@gmail.com>
2024-05-12 22:11:17 +0530

    update

--- REACT/practice/src/App.jsx
+++ REACT/practice/src/App.jsx
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
-const App = () => {
+const initialState = {
+  str: '',
+  currentInput: [],
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'SET_STR':
+      return {
+        ...state,
+        str: action.payload,
+      };
+    case 'APPEND':
+      return {
+        ...state,
+        currentInput: [...state.currentInput, state.str],
+        str: '',
+      };
+    case 'UNDO':
+      return {
+        ...state,
+        currentInput: state.currentInput.slice(0, -1),
+      };
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleInputChange = event => {
+    dispatch({ type: 'SET_STR', payload: event.target.value });
+  };
+
+  const handleAppend = () => {
+    dispatch({ type: 'APPEND' });
+  };
+
+  const handleUndo = () => {
+    dispatch({ type: 'UNDO' });
+  };
+
   return (
     <div>
-      
+      <input type="text" value={state.str} onChange={handleInputChange} />
+      <button onClick={handleAppend}>Append</button>
+      <button onClick={handleUndo}>Undo</button>
+      <div>
+        {state.currentInput.map((item, index) => {
+          return <span key={index}>{item} </span>;
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
