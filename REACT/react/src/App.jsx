bca7d2ad0b348f06e1d7de7a131bc6c5942fa8de
satyaki95 <www.satyaki95@gmail.com>
2024-05-13 20:41:31 +0530

    Nasted Map

--- REACT/react/src/App.jsx
+++ REACT/react/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import UNIQUE from './component/UNIQUE-ID/UNIQUE';
+import NESTED from './component/NASTED-MAPPING/NESTED';
 
 function App() {
 
   return (
     <div>
       <UNIQUE />
+      <NESTED />
     </div>
   )
 }
---
+++ REACT/react/src/component/NASTED-MAPPING/NESTED.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+const user = [
+    {
+        fullName: "Satyaki Saha",
+        age: 30,
+        phones: [
+            {
+                home: "7894561230"
+            },
+            {
+                office: "321654987"
+            }
+        ]
+    },
+    {
+        fullName: "Guddu Saha",
+        age: 20,
+        phones: [
+            {
+                home: "8894561230"
+            },
+            {
+                office: "421654987"
+            }
+        ]
+    },
+    {
+        fullName: "Pappu Saha",
+        age: 10,
+        phones: [
+            {
+                home: "9894561230"
+            },
+            {
+                office: "521654987"
+            }
+        ]
+    },
+    {
+        fullName: "Babu Saha",
+        age: 31,
+        phones: [
+            {
+                home: "0894561230"
+            },
+            {
+                office: "621654987"
+            }
+        ]
+    }
+]
+
+const NESTED = () => {
+  return (
+    <div>
+      {
+        user.map((users, index) => <article key={index}>
+            <h3>{users.fullName}</h3>
+            <p>{users.age}</p>
+            <p>{users.phones.map((phone,index) => <p key={index}>
+                <h5>{phone.office}</h5>
+                <h5>{phone.home}</h5>
+            </p>)}</p>
+        </article>)
+      }
+    </div>
+  );
+}
+
+export default NESTED;
