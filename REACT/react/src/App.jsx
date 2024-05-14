9767d13a5f8c8d7baa38355454d5d2248cc7e148
satyaki95 <www.satyaki95@gmail.com>
2024-05-14 18:23:26 +0530

    class

--- REACT/react/src/App.jsx
+++ REACT/react/src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import UNIQUE from './component/UNIQUE-ID/UNIQUE';
 import NESTED from './component/NASTED-MAPPING/NESTED';
+import CLASS from './component/CLASS-COMPONENT/CLASS';
 
 function App() {
 
   return (
     <div>
-      <UNIQUE />
+      {/* <UNIQUE />
       <NESTED />
+      <CLASS /> */}
     </div>
   )
 }
---
+++ REACT/react/src/component/CLASS-COMPONENT/CLASS.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import CLASS_PROPS from './CLASS_PROPS';
+
+export default function CLASS() {
+  return (
+    <div>
+      <CLASS_PROPS name="card1" desc="this is a card" />
+    </div>
+  )
+}
