a15956f40a624ae491a9c1d31407e077942dd1bb
satyaki95 <www.satyaki95@gmail.com>
2024-05-17 14:54:47 +0530

    dsa

---
+++ DSA in Javascript/Array/2nd.js
@@ -0,0 +1,34 @@
+//  Have the function ArrayChallenge(strArr) take the array of strings stored in strArr, 
+// which will contain pairs of integers in the following format: (i1,i2), 
+// where i1 represents a child node in a tree and the second integer i2 signifies that it is the parent of i1. 
+// For example: if strArr is ["(1,2)", "(2,4)", "(7,2)"], then this forms the following tree:
+// which you can see forms a proper binary tree. Your program should, in this case, 
+// return the string true because a valid binary tree can be formed. 
+// If a proper binary tree cannot be formed with the integer pairs, 
+// then return the string false. All of the integers within the tree will be unique, 
+// which means there can only be one node in the tree with the given integer value.
+// Examples
+// Input: ["(1,2)", "(2,4)", "(5,7)", "(7,2)", "(9,5)"]
+// Output: true
+// Input: ["(1,2)", "(3,2)", "(2,12)", "(5,2)"]
+// Output: false
+function ArrayChallenge(strArr) {
+  var varOcg = new Map();
+  var roots = new Set();
+
+  for (var pair of strArr) {
+    var [child, parent] = pair.slice(1, -1).split(",").map(Number);
+    if (varOcg.has(parent) && varOcg.get(parent).length === 2) {
+      return "false";
+    }
+    varOcg.set(parent, (varOcg.get(parent) || []).concat(child));
+    roots.delete(child);
+    roots.add(parent);
+  }
+
+  return roots.size === 3 ? "true" : "false";
+}
+
+// Test the function with the provided examples
+console.log(ArrayChallenge(["(1,2)", "(2,4)", "(5,7)", "(7,2)", "(9,5)"])); // Expected output: 'true'
+console.log(ArrayChallenge(["(1,2)", "(3,2)", "(2,12)", "(5,2)"])); // Expected output: 'false'
